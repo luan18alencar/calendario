243422287f2421225d79aeea76a9ab81dce51e2c
luan18alencar <luan.alencar@alunoescolassociais.org.br>
2024-04-05 07:31:01 -0300

    mlimita o numero de cores para 3 no javascript

--- calendar.js
+++ calendar.js
@@ -1,17 +1,21 @@
 
-function colorirDia(){
-    let days = document.getElementById('day').value;
+function colorirDia() {
+    let day = document.getElementById('day').value;
     let color = document.getElementById('color').value;
     let calendar = document.getElementById('calendar');
-    let td = calendar.getElementsByTagName('td')
-    [(parseInt (days)+2)];
- 
-    if(days <= 0){
-        alert("Digite um número positivo de dias(maior que 0).");
-        return;
+    let cells = calendar.querySelectorAll('td'); 
+  
+    
+    if (isNaN(day) || day <= 0 || day > 31) {
+      alert("Digite um número entre 1 e 31.");
+      return;
     }
-    days = Math.max(1, parseInt(days));
- 
-    td.style.backgroundColor = color;
-}
+    day = Math.max(1, parseInt(day));
+  
+    if (color >= 3) {
+      alert(`A cor ${color} já foi utilizada 3 vezes nesse dia.`);
+      return;
+    }
+   
+  }
 
